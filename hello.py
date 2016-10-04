343da6748fef3c600df4726bb9e12d95c08b87b5
serdarkacka <serdarkacka@gmail.com>
2016-10-04 16:26:04 +0300

    Use sum from demo.py

--- hello.py
+++ hello.py
@@ -1,9 +1,40 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from algorithm import *
+
+import yaml
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello World!"
+import logging
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/compute', methods=['GET', 'POST'])
+def compute():
+	if request.method == 'GET':
+		    return render_template('compute.html', result=0)
+	else:
+		input1 = request.form['input1']
+		app.logger.debug(input1)
+        
+
+       
+
+        yamlInput1 = yaml.safe_load(input1)
+        app.logger.debug(yamlInput1)
+        print 'yamlInput1: ' + str(yamlInput1)
+        
+
+        result = sum(yamlInput1)
+        print result
+        return render_template('compute.html', result=result)
 
 if __name__ == "__main__":
 	app.run()
